8e8d54d11c4df98ffefeade606658aebe0849566
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-10 15:11:27 -0600

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,14 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ashley Davis";
+    int age = 37;
+    String state = "Utah";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+
+    /*
+      Your solution goes here
+    */
+  }
+}
