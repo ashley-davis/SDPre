08a37850f3bdeac4bb5386c4fdc45850024bfd5e
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-10 22:00:06 -0600

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,27 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+
+
+    double num1;
+    num1 = 15;
+
+    double num2;
+    num2 = 6;
+
+    double num3;
+    num3 = 2;
+
+    double total;
+    total = num1 + num2 + num3;
+
+    double average;
+    average = total/ 3;
+
+    System.out.println(average);
+
+
+    /*
+      Your solution goes here
+    */
+  }
+}
