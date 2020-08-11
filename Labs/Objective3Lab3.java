9d9fe8204c53fb0c6f9a8bccce4db4e8d66e8c41
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-11 01:06:18 -0600

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,23 @@
+public class Objective3Lab3 {
+  public static void main(String[] args) {
+int first;
+first = 1 + (int)(Math.random() * 6);
+
+int second;
+second = 1 + (int)(Math.random() * 6);
+
+int third;
+third = 1 + (int)(Math.random() * 6);
+
+int sum;
+sum = (first + second + third);
+
+
+System.out.println(sum);
+
+
+
+  }
+
+
+}
