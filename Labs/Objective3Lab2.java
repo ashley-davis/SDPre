39559d0c63e2d61e0d09b7df62b3f85a04b053bb
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-11 00:48:28 -0600

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,25 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1;
+    side1 = (10.0 * 10.0);
+
+    double side2;
+    side2 = (8.0 * 8.0);
+
+    double result;
+    result = side1 + side2;
+
+    double hypotenuse;
+    hypotenuse = Math.sqrt(164);
+
+
+    System.out.println(hypotenuse);
+
+
+
+
+
+  }
+
+
+}
