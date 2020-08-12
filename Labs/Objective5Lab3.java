8f580d6842f9a3237a13c3671a591f70f7ff8d9f
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-12 13:54:54 -0600

    complete Objective5Lab3

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,26 @@
+public class Objective5Lab3 {
+    public static void main(String[] args) {
+
+java.util.Scanner scanner = new java.util.Scanner(System.in);
+System.out.println("Please enter a number: ");
+int userNum = scanner.nextInt();
+
+if (userNum < 0){
+  System.out.println( userNum + " is less than " + "0");
+}
+
+else if(userNum > 0){
+  System.out.println( userNum + " is greater than " + "0");
+}
+
+else if(userNum == (0)) {
+  System.out.println( userNum + " is equal to " + "0");
+}
+
+
+      /*
+        Your solution goes here
+      */
+
+  }
+}
