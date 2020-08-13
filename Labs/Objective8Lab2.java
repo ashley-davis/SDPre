a44e6a373b7c16bd08c2a2f551ffdd3221cdcd8c
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-13 14:58:02 -0600

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+
+  int x;
+  int total = 0;
+  for (x=1; x <= 20; x++){
+    total += x;
+  }
+System.out.println(total);
+}
+}
