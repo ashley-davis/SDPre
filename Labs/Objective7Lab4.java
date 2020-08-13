1eea82e8f108b30f94d5c6560e7e4eca215ee656
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-13 12:33:24 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,23 @@
+//A Java program to compute sum of digits 1- 20
+
+public class Objective7Lab4 {
+  public static void main(String[] args){
+
+    int counter = 0;
+    int sum = 0;
+
+
+    while(counter <= 19) {
+      counter = counter + 1;
+    sum = sum + counter;
+
+    System.out.println(sum);
+  }
+
+
+
+
+
+  }
+
+}
