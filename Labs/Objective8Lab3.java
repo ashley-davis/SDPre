211b843a3bb600a9cd6e59381e8ea0884717c468
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-14 09:05:54 -0600

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,22 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter;
+    int i;
+
+for (i = 0; i <= 20; i+=1)
+{
+  if (i % 2 == 0)
+  {
+    System.out.println(i + " is even ");
+  }
+  if (i % 2 != 0)
+  {
+    System.out.println(i + " is odd ");
+  }
+
+}
+    /*
+      Your solution goes here
+    */
+  }
+}
