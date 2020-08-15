605f2f8aca3f5db3fce71e014d884f4a9eeeb4cd
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-14 19:00:33 -0600

    complete objective9lab4

---
+++ Labs/Objective9Lab4.java
@@ -0,0 +1,24 @@
+public class Objective9Lab4 {
+  public static void main(String[] args) {
+
+java.util.Scanner scanner = new java.util.Scanner(System.in);
+int selection = scanner.nextInt();
+double num1, num2;
+
+System.out.println("Please give me a number: ");
+            num1 = scanner.nextInt();
+            System.out.println("Please give me another number: ");
+            num2 = scanner.nextInt();
+
+double average = average(num1, num2);
+System.out.println("The average of " + num1 + "and" + num2 + "is: " + average);
+
+scanner.close();
+
+
+  }
+
+  /*
+  Write/define the "findAverage" method here
+  */
+}
